b75a981a1d110e13f54446e491312c6dbbf508a2
david1488tv <37955716+david1488tv@users.noreply.github.com>
2018-07-05 15:41:12 +0300

    Изменил запрос на выборку данных

--- node/controllers/db/select.js
+++ node/controllers/db/select.js
@@ -1,12 +1,12 @@
 const sequelize = require('../../models/sequelize');
 
-//const {News} = require('../../models/index');
-
 module.exports = {
     selecter: async function (req, res) {
         const {body} = req;
         try {
-            const news = (sequelize.query('SELECT * FROM news_temp', { type: sequelize.QueryTypes.SELECT}))
+            const news = (sequelize.query('SELECT DISTINCT news.id, news.name, news.content \
+            FROM news INNER JOIN news_temp ON news.id = news_temp.news_id', 
+            { type: sequelize.QueryTypes.SELECT}))
             .then(news => {
                 console.log('get data success!');
                 res.status(200).json({
