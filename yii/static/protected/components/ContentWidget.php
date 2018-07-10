cab1fee9e13d11f4a8b4ddf4559974df0b28cbed
cast98 <kostyawwe98@gmail.com>
2018-07-10 13:52:04 +0300

    допилил виджет

--- yii/static/protected/components/ContentWidget.php
+++ yii/static/protected/components/ContentWidget.php
@@ -3,16 +3,27 @@ Yii::import('system.web.widgets.CWidget');
 
 class ContentWidget extends CWidget {
 
+    public $cssFile;
     public $htmlOptions = array();
 
     public function init(){
         parent::init();
+        $this->cssFile = Yii::app()->getBaseUrl(true).'/css/list.css';
         if(!isset($this->htmlOptions['id'])) {
             $this->htmlOptions['id'] = 'widget';
         }
     }
 
     public function run() {
+        $this->registerClientScript();
         echo CHtml::tag('div', $this->htmlOptions);
     }
+
+    public function registerClientScript()
+	{
+        if($this->cssFile!==false)
+        {
+            Yii::app()->getClientScript()->registerCssFile($this->cssFile);
+        }
+	}
 }
